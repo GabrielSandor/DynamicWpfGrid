3f7c6f91c85f59de9bf37b3a39d9ceb7352d6be4
Gabriel Sandor <gabi.t.sandor@gmail.com>
2020-01-27 22:02:26 +0200

    dynamic grid row and column definitions

--- DynamicWpfGrid/MainWindowViewModel.cs
+++ DynamicWpfGrid/MainWindowViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace DynamicWpfGrid
 {
     public class MainWindowViewModel
     {
         public ObservableCollection<CustomControl> CustomControls { get; set; }
+
+        public int Rows => CustomControls.Max(c => (c.DataContext as CustomControlViewModel).Row + 1);
+        public int Columns => CustomControls.Max(c => (c.DataContext as CustomControlViewModel).Column + 1);
 
         public MainWindowViewModel()
         {
